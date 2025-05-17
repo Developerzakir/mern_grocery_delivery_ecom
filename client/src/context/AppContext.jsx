a1e19c62d806b3db6288599d5e041e025128df7f
zakir Hossen <zakirhossen082@gmail.com>
2025-05-17 16:56:10 +0600

    cart items count update & cart icon display on mobile view

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -57,6 +57,27 @@ export const AppContextProvider = ({children})=>{
         setCartItems(cartData);
     }
 
+    //Get cart item count
+    const getCartCount = ()=>{
+        let totalCount = 0;
+        for(const item in cartItems){
+            totalCount += cartItems[item];
+        }
+        return totalCount;
+    }
+
+    //Get cart total amount
+    const getCartAmount = ()=>{
+        let totalAmount = 0;
+        for(const items in cartItems){
+            let itemInfo = products.find((product)=>product._id === items);
+            if(cartItems[items] > 0){
+                totalAmount += itemInfo.offerPrice * cartItems[items]
+            }
+        }
+        return Math.floor(totalAmount * 100) / 100;
+    }
+
     useEffect(()=>{
         fetchProducts();
     },[]);
@@ -76,7 +97,9 @@ export const AppContextProvider = ({children})=>{
         removeFromCart,
         cartItems,
         searchQuery,
-        setSearchQuery
+        setSearchQuery,
+        getCartCount,
+        getCartAmount
     }
 
     return <AppContext.Provider value={value}>

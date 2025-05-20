441cd4a3968df7f2c15ecd2958a8fee06e5cd9a5
zakir Hossen <zakirhossen082@gmail.com>
2025-05-20 18:49:01 +0600

    db connection setup

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
+import cors from 'cors';
+import connectDB from './configs/db.js';
+import  'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+
+//database config
+connectDB();
+
+
+
+//Allowed multiple origins
+const allowedOrigins = ['http://localhost:5173']
+
+//Middleware Configuaration
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({origin:allowedOrigins, credential:true}))
+
 
 app.get('/', (req,res)=>res.send('Api is working'));
 
